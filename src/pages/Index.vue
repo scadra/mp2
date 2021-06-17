8c6c75ba4dcd93c9ed6e6cd48210842467418237
Patrice Jacquot <patrice.jacquot@luxhub.com>
2021-06-17 10:36:22 +0200

    MP2-88 Display version on homepage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,3 +1,12 @@
 <template src="Pages/home/home.template.html"></template>
 <script src="Pages/home/home.component.ts" lang="ts"></script>
 <style src="Pages/home/home.style.scss" lang="scss" scoped></style>
+<static-query>
+query {
+  metadata {
+    siteName,
+    siteDescription,
+    siteVersion
+  }
+}
+</static-query>
