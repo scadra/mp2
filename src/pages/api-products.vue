4bf916ae236e7c419625e9bec6cae855110ecf0c
Giuseppe <giuseppe.impicciche@externalstaff.luxhub.com>
2021-06-21 11:07:49 +0200

    MP2-109 Created new API list page with new menu entry

---
+++ src/pages/api-products.vue
@@ -0,0 +1,3 @@
+<template src="Pages/api-products/api-products.template.html"></template>
+<script src="Pages/api-products/api-products.component.ts" lang="ts"></script>
+<style src="Pages/api-products/api-products.style.scss" lang="scss" scoped></style>
