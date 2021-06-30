b9cd031c18ac3d144e8048d3ac83f090ca9c5652
Giuseppe <giuseppe.impicciche@externalstaff.luxhub.com>
2021-06-30 11:33:32 +0200

    feat/MP2-108 added providers filter in api-products page

---
+++ src/components/api-products/api-filter/api-filter.component.ts
@@ -0,0 +1,63 @@
+// Add dependencies
+import Vue from "vue";
+import { Component, Prop } from "vue-property-decorator";
+//Components
+//Models
+import { Provider } from "Models/api/provider.model";
+
+/**
+ * Controller of api-list-container
+ * @Component
+ */
+@Component({
+  components: {},
+})
+export default class ApiFilter extends Vue {
+  @Prop() filterList: Provider[];
+  @Prop() placeholder: string;
+  @Prop() label: string;
+  @Prop() icon: string;
+
+  selectedFilter: Provider[] = [];
+  filterListComponent: Provider[] = [];
+  focus = false;
+
+  mounted(): void {
+    document.addEventListener("click", (e) => {
+      if (
+        e.target != this.$el.querySelector("#search") &&
+        e.target != this.$el.querySelector(".filterList")
+      ) {
+        this.focus = false;
+      }
+    });
+
+    this.filterListComponent = this.filterList;
+  }
+
+  removeSelectedTag(tag: Provider): void {
+    this.filterListComponent.push(tag);
+    this.selectedFilter = this.selectedFilter.filter((e) => e.id != tag.id);
+    this.$emit("filterChange", this.selectedFilter);
+  }
+
+  searchKey(searchKey: string): void {
+    this.filterListComponent = this.filterList.filter((e) =>
+      e.name.toLowerCase().includes(searchKey.toLowerCase())
+    );
+  }
+
+  clearFilters(): void {
+    this.filterListComponent = this.filterList;
+    this.selectedFilter = [];
+    this.$emit("filterChange", this.selectedFilter);
+  }
+
+  addFilterTag(filter: Provider): void {
+    this.filterListComponent = this.filterListComponent.filter(
+      (e) => e.id != filter.id
+    );
+    this.selectedFilter.push(filter);
+    this.$emit("filterChange", this.selectedFilter);
+  }
+}
