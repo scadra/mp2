2abea000de10c75776bade1adf83721e6ad6fbd5
Giuseppe <giuseppe.impicciche@externalstaff.luxhub.com>
2021-06-30 18:38:25 +0200

    feat/MP2-110 Created Support menu item, added support component page and register component page with work in progress image

---
+++ src/pages/support.vue
@@ -0,0 +1,3 @@
+<template src="Pages/support/support.template.html"></template>
+<script src="Pages/support/support.component.ts" lang="ts"></script>
+<style src="Pages/support/support.style.scss" lang="scss" scoped></style>
---
+++ src/pages/support/support.component.ts
@@ -0,0 +1,15 @@
+// Dependencies
+import { Component } from "vue-property-decorator";
+import Vue from "vue";
+
+/**
+ * The support page
+ * @class
+ */
+@Component({
+  metaInfo: {
+    title: "support",
+  },
+  components: {},
+})
+export default class Support extends Vue {}
